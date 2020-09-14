f6cd0c955096757f2b3f9e033ec5273e4a4fb792
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-09-14 19:12:46 +0200

    Adding Exception

---
+++ src/main/java/com/flights/exceptions/AircraftNotFoundException.java
@@ -0,0 +1,8 @@
+package com.flights.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Aircraft not found!")
+public class AircraftNotFoundException extends Exception{
+}
